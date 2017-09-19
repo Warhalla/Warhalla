c609f0026470e5c7c65136b8f45cac71fbdff08e
benzen89 <salomaa.leo@gmail.com>
2017-09-19 14:50:18 +0300

    Bard added
    Bard prefab added
    Bard moves OnBeat
    Main camera childed to bard

---
+++ Warhalla/Assets/BardMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BardMovement : MonoBehaviour {
+
+	public float bard_move_lenth;
+	public float bard_move_speed;
+	private float bard_x_from;
+	private float bard_x_to;
+	private float start_time;
+	private float timer;
+
+	void Start () {
+		MusicManager.OnBeat += BardMove;
+	}
+
+	void Update () {
+		//this.transform.position = new Vector2(Mathf.Lerp(bard_x_from, bard_x_to, Time.time - start_time), this.transform.position.y);
+		this.transform.position = new Vector2(Mathf.Lerp(bard_x_from, bard_x_to, timer), this.transform.position.y);
+		timer += Time.deltaTime * bard_move_speed;
+	}
+
+	void BardMove(){
+		bard_x_from = this.transform.position.x;
+		bard_x_to = bard_x_from + bard_move_lenth;
+		start_time = Time.time;
+		timer = 0;
+	}
+
+}
