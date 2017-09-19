5180c17dd784064c80f9556098b85b737a7835d0
benzen89 <salomaa.leo@gmail.com>
2017-09-19 16:36:15 +0300

    Instrument list added
    Instrument list generates a list of random instruments and spawns corresponding vikings
    Viking prefabs added

--- Warhalla/Assets/Scripts/InstrumentList.cs
+++ Warhalla/Assets/Scripts/InstrumentList.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class InstrumentList : MonoBehaviour {
 
+	private float vikingSpawnX;
+
+	public GameObject viking_flute;
+	public GameObject viking_harp;
+	public GameObject viking_horn;
+
 	void Start () {
 		List<Instrument> instrument_order = new List<Instrument>();
 
 		instrument_order.Add( Instrument.None);
 		instrument_order.Add( Instrument.None);
 		instrument_order.Add( Instrument.None);
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 40; i++) {
 			instrument_order.Add( RandomInstrument());
 		}
 		for (int i = 0; i < instrument_order.Count; i++) {
-			Debug.Log (instrument_order[i]);
+			if (instrument_order [i] == Instrument.Flute) {
+				Instantiate (viking_flute, new Vector2 (i * 2, 0), Quaternion.identity);
+			}
+			if (instrument_order [i] == Instrument.Harp) {
+				Instantiate (viking_harp, new Vector2 (i * 2, 0), Quaternion.identity);
+			}
+			if (instrument_order [i] == Instrument.Horn) {
+				Instantiate (viking_horn, new Vector2 (i * 2, 0), Quaternion.identity);
+			}
 		}
 	}
 
