cbece870d6734ec569a096587b4b8d6d6faf7df3
benzen89 <salomaa.leo@gmail.com>
2017-09-19 20:21:31 +0300

    Added button stretch and shrink

---
+++ Warhalla/Assets/Scripts/ButtonFeedbackManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFeedbackManager : MonoBehaviour {
+
+	public GameObject selectedButton;
+	public GameObject previousSelectedButton;
+
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (selectedButton);
+	}
+}
