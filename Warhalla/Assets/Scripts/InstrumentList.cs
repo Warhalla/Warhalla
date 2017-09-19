fb5c2608a1824cf9f68fbf57db791e732d567c62
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2017-09-19 17:36:30 +0300

    infinite level

--- Warhalla/Assets/Scripts/InstrumentList.cs
+++ Warhalla/Assets/Scripts/InstrumentList.cs
@@ -24,21 +24,27 @@ public class InstrumentList : MonoBehaviour {
 			instrument_order.Add( RandomInstrument());
 		}
 		for (int i = 0; i < instrument_order.Count; i++) {
-			if (instrument_order [i] == Instrument.Flute) {
-				Instantiate (viking_flute, new Vector2 (i * 2, 0), Quaternion.identity);
-			}
-			if (instrument_order [i] == Instrument.Harp) {
-				Instantiate (viking_harp, new Vector2 (i * 2, 0), Quaternion.identity);
-			}
-			if (instrument_order [i] == Instrument.Horn) {
-				Instantiate (viking_horn, new Vector2 (i * 2, 0), Quaternion.identity);
-			}
+			SpawnViking(i);
 		}
 		MusicManager.OnBar += IncrementIndex;
 	}
 
+	private void SpawnViking(int i){
+		if (instrument_order [i] == Instrument.Flute) {
+			Instantiate (viking_flute, new Vector2 (i * 2, 0), Quaternion.identity);
+		}
+		if (instrument_order [i] == Instrument.Harp) {
+			Instantiate (viking_harp, new Vector2 (i * 2, 0), Quaternion.identity);
+		}
+		if (instrument_order [i] == Instrument.Horn) {
+			Instantiate (viking_horn, new Vector2 (i * 2, 0), Quaternion.identity);
+		}
+	}
+
 	private void IncrementIndex(){
 		index++;
+		instrument_order.Add( RandomInstrument());
+		SpawnViking(instrument_order.Count - 1);
 		music.CheckInstrument();
 		print(GetCurrentViking());
 	}
