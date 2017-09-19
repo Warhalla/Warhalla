16dd408f0698a9603003c5c4937c2847eeadf2eb
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2017-09-19 12:54:58 +0300

    added basic dumb beat system

---
+++ Warhalla/Assets/BlinkOnBeat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkOnBeat : MonoBehaviour {
+
+	private Button button;
+
+	// Use this for initialization
+	void Start () {
+		button = GetComponent<Button>();
+		MusicManager.OnBeat += Blink;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Blink(){
+		button.interactable = !button.interactable;
+	}
+
+	void OnDestroy()
+	{
+		MusicManager.MakeBeat -= Blink;
+	}
+}
---
+++ Warhalla/Assets/MusicManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	public delegate void BeatAction();
+    public static event BeatAction OnBeat;
+
+	private AudioSource backgroundMusic;
+	private bool loopedQuant = false;
+	private bool loopedBeat = false;
+
+	// Use this for initialization
+	void Start () {
+		backgroundMusic = GetComponent<AudioSource>();
+		StartCoroutine(BeatEverySecond());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator BeatEverySecond(){
+		while(true){
+			yield return new WaitForSeconds(1f);
+			print("beat");
+			OnBeat();
+		}
+	}
+
+	void CheckBeat(){
+		if ((Mathf.Min(backgroundMusic.timeSamples % 12000, Mathf.Abs((backgroundMusic.timeSamples % 12000) - 12000)) < 2000) && !loopedQuant) {
+			loopedQuant = true;
+			//beat.ReportHalfBeat();
+		}
+		if (backgroundMusic.timeSamples % 12000 > 6000 && Mathf.Abs((backgroundMusic.timeSamples % 12000) - 12000) > 4000) {
+			loopedQuant = false;
+		}
+		
+		if ((Mathf.Min(backgroundMusic.timeSamples % 24000, Mathf.Abs((backgroundMusic.timeSamples % 24000) - 24000)) < 2000) && !loopedBeat) {
+			loopedBeat = true;
+			OnBeat();
+		}
+		if (backgroundMusic.timeSamples % 24000 > 12000 && Mathf.Abs((backgroundMusic.timeSamples % 24000) - 24000) > 4000) {
+			loopedBeat = false;
+		}	
+	}
+}
