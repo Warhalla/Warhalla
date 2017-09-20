b9f6d2a26e7fb0561e460ad7858f1f7f0445fd10
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2017-09-20 12:30:11 +0300

    starting viking animations

---
+++ Warhalla/Assets/AnimateToMusic.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateToMusic : MonoBehaviour {
+
+	private Sprite[] hitSprites;
+	int hitIndex = 0;
+	private Sprite[] moveSprites;
+	int moveIndex = 0;
+	private SpriteRenderer image;
+	private bool walking = false;
+	public string pathToFolders;
+	public int variation;
+
+	// Use this for initialization
+	void Start () {
+		image = GetComponent<SpriteRenderer>();
+		hitSprites = Resources.LoadAll<Sprite>("Vikings/" + pathToFolders + variation + "/Hit");
+		moveSprites = Resources.LoadAll<Sprite>("Vikings/" + pathToFolders + variation + "/Move");
+		MusicManager.OnBeat += ProgressAnimation;
+		hitIndex = Random.Range(0,4);
+	}
+
+
+	void ProgressAnimation(){
+		if(!walking){
+			hitIndex++;
+			image.sprite = hitSprites[hitIndex % 4];
+		} else {
+
+		}
+	}
+
+	void StartWalking(){
+		walking = true;
+	}
+	
+	/// <summary>
+	/// This function is called when the MonoBehaviour will be destroyed.
+	/// </summary>
+	void OnDestroy()
+	{
+		MusicManager.OnBeat -= ProgressAnimation;
+	}
+}
