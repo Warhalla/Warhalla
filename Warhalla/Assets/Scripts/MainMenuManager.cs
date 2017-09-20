7949fe6160e89c354eefdf0228d8351e7673dc50
benzen89 <salomaa.leo@gmail.com>
2017-09-20 10:45:51 +0300

    Fancier main menu

--- Warhalla/Assets/Scripts/MainMenuManager.cs
+++ Warhalla/Assets/Scripts/MainMenuManager.cs
@@ -6,20 +6,25 @@ public class MainMenuManager : MonoBehaviour {
 	float timer;
 	bool startgame;
 	float textLift;
+	Vector3 backgroundPos;
 
 	public Camera maincam;
 	public GameObject fadePlane;
 	public GameObject txt;
+	public GameObject menuBackground;
 
 	void Start(){
 		startgame = false;
 		textLift = txt.transform.GetComponent<RectTransform> ().position.y;
+		backgroundPos = menuBackground.transform.position;
 	}
 
 	void Update () {
 		if (Input.anyKey) {
 			startgame = true;
 		}
+		backgroundPos = new Vector3 (backgroundPos.x + Time.deltaTime * -0.2f, backgroundPos.y, backgroundPos.z);
+		menuBackground.transform.position = backgroundPos;
 		if (startgame) {
 			timer += Time.deltaTime;
 			maincam.GetComponent<Camera> ().orthographicSize -= Time.deltaTime * 0.5f;
