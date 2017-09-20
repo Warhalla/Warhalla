e257c95078230b6cc96f10c7d9a1e68e92297bd9
benzen89 <salomaa.leo@gmail.com>
2017-09-20 10:26:59 +0300

    Main menu press effects added
    Fades to white and text floats up

--- Warhalla/Assets/Scripts/MainMenuManager.cs
+++ Warhalla/Assets/Scripts/MainMenuManager.cs
@@ -5,12 +5,15 @@ public class MainMenuManager : MonoBehaviour {
 
 	float timer;
 	bool startgame;
+	float textLift;
 
 	public Camera maincam;
-	public GameObject background;
+	public GameObject fadePlane;
+	public GameObject txt;
 
 	void Start(){
 		startgame = false;
+		textLift = txt.transform.GetComponent<RectTransform> ().position.y;
 	}
 
 	void Update () {
@@ -19,7 +22,15 @@ public class MainMenuManager : MonoBehaviour {
 		}
 		if (startgame) {
 			timer += Time.deltaTime;
-			maincam.GetComponent<Camera> ().orthographicSize -= Time.deltaTime;
+			maincam.GetComponent<Camera> ().orthographicSize -= Time.deltaTime * 0.5f;
+			Color tmp = fadePlane.GetComponent<SpriteRenderer>().color;
+			tmp.a = timer;
+			fadePlane.GetComponent<SpriteRenderer>().color = tmp;
+
+			textLift += timer * 1;
+			Vector3 txtPos = new Vector3 (315.5f, textLift, 0);
+			txt.transform.GetComponent<RectTransform> ().position = txtPos;
+
 			if (timer > 1f) {
 				SceneManager.LoadScene ("Game", LoadSceneMode.Single);
 			}
